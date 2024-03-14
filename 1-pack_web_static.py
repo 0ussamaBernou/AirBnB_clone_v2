de13700af7b363886ad83886a721a41be2b6f0f1
0ussamabernou <oussama.bernou@outlook.com>
2024-03-14 11:30:22 +0000

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from fabric.api import run
+from fabric.api import local
 import datetime
 
 # Get current datetime object
@@ -11,10 +11,10 @@ def do_pack():
     Generates a .tgz archive from the contents of the web_static folder of the AirBnB Clone repo
     """
 
-    run("mkdir -p ./versions")
+    local("mkdir -p ./versions")
     time = f"{now.year}{now.month}{now.day}{now.hour}{now.minute}{now.second}"
     archive = f"./versions/web_static_{time}.tgz"
-    code = run(
+    code = local(
         f"tar -cvzf {archive} /data/web_static",
         capture=True,
     )
