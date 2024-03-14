de77b1c1ec1d7550170a43edd948ce7924942f64
0ussamabernou <oussama.bernou@outlook.com>
2024-03-14 11:42:49 +0000

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from fabric.api import local
+from fabric.api import local, task
 import datetime
 
 # Get current datetime object
 now = datetime.datetime.now()
 
 
+@task
 def do_pack():
     """
     Generates a .tgz archive from the contents of
