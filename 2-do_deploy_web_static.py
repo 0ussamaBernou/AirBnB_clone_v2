af7c3aa98cc1d9613f955d4af4173765543c8162
0ussamabernou <oussama.bernou@outlook.com>
2024-03-14 13:51:24 +0000

    update

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to web servers
+"""
+import os
+from fabric.api import env, put, run
+
+# Replace with IP addresses of your web servers
+env.hosts = [
+    "web-01.bernoussama.tech",
+    "web-02.bernoussama.tech",
+]
+# Replace with the username for accessing your web servers
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to web servers
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        archive_name = os.path.splitext(archive_path)[0]
+        destination = f"/data/web_static/releases/{archive_name}"
+
+        put(archive_path, "/tmp")
+        run(f"tar -xzf /tmp/{archive_name}.tgz -C {destination} ")
+        run(f"rm /tmp/{archive_name}.tgz")
+        run("rm -rf /data/web_static/current")
+        run(f"ln -s {destination} /data/web_static/current")
+
+        return True
+    except Exception:
+        return False
