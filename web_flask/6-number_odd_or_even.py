7adc08e964f7b2d235d05659073f7d436821e0e6
0ussamabernou <oussama.bernou@outlook.com>
2024-03-24 01:25:57 +0000

    update

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Flask web server
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,37 +10,58 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """
+    Hello route
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """
+    HBNB route
+    """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
+    """
+    C route
+    """
     return f"C {text.replace('_', ' ')}"
 
 
 @app.route("/python", strict_slashes=False, defaults={"text": "is cool"})
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
+    """
+    Python route
+    """
     return f"Python {text.replace('_', ' ')}"
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_route(n):
+    """
+    Number routej
+    """
     return f"{n} is a number"
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
+    """
+    Number template route
+    """
     return render_template("5-number.html", n=n)
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
+    """
+    Number odd or even route
+    """
     parity = "even" if n % 2 == 0 else "odd"
     return render_template("6-number_odd_or_even.html", n=n, p=parity)
 
