e15a88230e790a533c8fff3f69d818bd4b68cdfc
0ussamabernou <oussama.bernou@outlook.com>
2024-03-24 01:31:42 +0000

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,6 +10,64 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    Hello route
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    HBNB route
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    C route
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python", strict_slashes=False, defaults={"text": "is cool"})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """
+    Python route
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    """
+    Number routej
+    """
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """
+    Number template route
+    """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    Number odd or even route
+    """
+    parity = "even" if n % 2 == 0 else "odd"
+    return render_template("6-number_odd_or_even.html", n=n, p=parity)
+
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """
