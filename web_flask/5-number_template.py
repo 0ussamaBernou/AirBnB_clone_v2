4727a2c77f7cf302bc33fb035441ccdcca87e353
0ussamabernou <oussama.bernou@outlook.com>
2024-03-24 01:22:13 +0000

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,32 +10,38 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
+    """Displays 'C' followed by the value of the text variable"""
     return f"C {text.replace('_', ' ')}"
 
 
 @app.route("/python", strict_slashes=False, defaults={"text": "is cool"})
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
+    """Displays 'Python' followed by the value of the text variable"""
     return f"Python {text.replace('_', ' ')}"
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_route(n):
+    """Displays 'n is a number' only if n is an integer"""
     return f"{n} is a number"
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
+    """Displays an HTML page only if n is an integer"""
     return render_template("5-number.html", n=n)
 
 
