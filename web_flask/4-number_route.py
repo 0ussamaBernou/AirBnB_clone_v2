b2a68214f93dbd7478e0869633489f7f83579f57
0ussamabernou <oussama.bernou@outlook.com>
2024-03-24 01:20:53 +0000

    update

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Start a Flask web application
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,27 +10,32 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """Display Hello HBNB! on the route /"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Display HBNB on the route /hbnb"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
+    """Display C followed by the value of the text variable on the route /c/<text>"""
     return f"C {text.replace('_', ' ')}"
 
 
 @app.route("/python", strict_slashes=False, defaults={"text": "is cool"})
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
+    """Display Python followed by the value of the text variable on the route /python/<text>"""
     return f"Python {text.replace('_', ' ')}"
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_route(n):
+    """Display n is a number only if n is an integer"""
     return f"{n} is a number"
 
 
